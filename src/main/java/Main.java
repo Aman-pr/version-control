9c0657cfd9a17d50bdea4b86089e142b9ff78012
Aman-pr <amankhan2729@gmail.com>
2024-12-23 00:59:06 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,42 +3,44 @@ import java.nio.file.*;
 import java.security.*;
 import java.util.*;
 import java.util.zip.*;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class Main {
-    // Store our git folder name - we'll use this a lot
+    // Git folder name constant
     static final String GIT_FOLDER = ".git";
     
     public static void main(String[] args) {
-        // Show where logs will go
         System.err.println("Logs from your program will appear here!");
         
         try {
-            // Figure out what command to run
-            String whatToDo = args[0];
+            // Get the command from arguments
+            String command = args[0];
             
-            // Do different things based on the command
-            switch (whatToDo) {
+            // Handle different git commands
+            switch (command) {
                 case "init" -> makeNewRepo();
                 case "cat-file" -> showFileContents(args);
                 case "hash-object" -> makeHashFromFile(args);
                 case "ls-tree" -> showTreeContents(args);
                 case "write-tree" -> writeTreeObject();
-                default -> System.out.println("I don't know how to do that: " + whatToDo);
+                case "commit-tree" -> createCommit(args);
+                default -> System.out.println("I don't know how to do that: " + command);
             }
-        } catch (Exception oops) {
-            // If something goes wrong, let's say what happened
-            System.err.println("Uh oh, we hit a problem: " + oops.getMessage());
-            oops.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    // Make a new git repo
+    // Initialize a new git repository
     private static void makeNewRepo() throws IOException {
+        // Create .git directory structure
         File gitFolder = new File(GIT_FOLDER);
         new File(gitFolder, "objects").mkdirs();
         new File(gitFolder, "refs").mkdirs();
         
-        // Make the HEAD file point to main branch
+        // Create HEAD file pointing to main branch
         File headFile = new File(gitFolder, "HEAD");
         headFile.createNewFile();
         Files.write(headFile.toPath(), "ref: refs/heads/main\n".getBytes());
@@ -46,49 +48,48 @@ public class Main {
         System.out.println("Created a new git repo!");
     }
 
-    // Show what's in a git object
+    // Show contents of a git object
     private static void showFileContents(String[] args) throws IOException {
         String hash = args[2];
         String folder = hash.substring(0, 2);
         String file = hash.substring(2);
         
+        // Read the object file
         File objectFile = new File(String.format("%s/objects/%s/%s", GIT_FOLDER, folder, file));
+        byte[] content = readCompressedFile(objectFile);
         
-        // Read and uncompress the file
-        byte[] stuff = readCompressedFile(objectFile);
-        String content = new String(stuff, "UTF-8");
-        String result = content.replaceAll("^blob \\d+\\x00", "");
-        
+        // Remove the header and print content
+        String result = new String(content, "UTF-8").replaceAll("^blob \\d+\\x00", "");
         System.out.print(result);
     }
 
-    // Make a git object from a file
+    // Create a hash object from a file
     private static void makeHashFromFile(String[] args) throws IOException, NoSuchAlgorithmException {
         if (!args[1].equals("-w")) {
-            System.err.println("Sorry, you need to use -w with this command");
+            System.err.println("Please use -w with this command");
             return;
         }
         
+        // Read file content
         File inputFile = new File(args[2]);
         byte[] fileContent = Files.readAllBytes(inputFile.toPath());
         
-        // Add the git header
+        // Create git blob object
         String header = "blob " + fileContent.length + "\0";
         byte[] headerBytes = header.getBytes();
         
-        // Put it all together
+        // Combine header and content
         byte[] fullContent = new byte[headerBytes.length + fileContent.length];
         System.arraycopy(headerBytes, 0, fullContent, 0, headerBytes.length);
         System.arraycopy(fileContent, 0, fullContent, headerBytes.length, fileContent.length);
         
-        // Get the hash and save the object
+        // Save and print hash
         String hash = calculateSHA1(fullContent);
         saveCompressedFile(hash, fullContent);
-        
         System.out.println(hash);
     }
 
-    // Show what's in a tree object
+    // Show contents of a tree object
     private static void showTreeContents(String[] args) throws IOException {
         if (!args[1].equals("--name-only")) {
             System.err.println("Please use --name-only with this command");
@@ -99,44 +100,92 @@ public class Main {
         File treeFile = new File(String.format("%s/objects/%s/%s", 
             GIT_FOLDER, hash.substring(0, 2), hash.substring(2)));
 
+        // Read tree content
         byte[] treeContent = readCompressedFile(treeFile);
         
-        // Skip past the header to get to the good stuff
-        int position = 0;
-        while (treeContent[position] != 0) position++;
-        position++;
+        // Skip header
+        int pos = 0;
+        while (treeContent[pos] != 0) pos++;
+        pos++;
         
-        // Get all the file names
+        // Extract file names
         List<String> names = new ArrayList<>();
-        while (position < treeContent.length) {
-            while (treeContent[position] != ' ') position++;
-            position++;
+        while (pos < treeContent.length) {
+            // Skip mode
+            while (treeContent[pos] != ' ') pos++;
+            pos++;
             
+            // Get file name
             StringBuilder name = new StringBuilder();
-            while (treeContent[position] != 0) {
-                name.append((char)treeContent[position]);
-                position++;
+            while (treeContent[pos] != 0) {
+                name.append((char)treeContent[pos]);
+                pos++;
             }
             names.add(name.toString());
             
-            position += 21;  // skip past hash
+            // Skip hash (20 bytes)
+            pos += 21;
         }
         
-        // Show them in order
+        // Print sorted names
         Collections.sort(names);
         for (String name : names) {
             System.out.println(name);
         }
     }
 
-    // Write a tree object for the current directory
+    // Create a tree object for current directory
     private static void writeTreeObject() throws IOException, NoSuchAlgorithmException {
-        // Get everything in the current directory
         String treeHash = writeTreeForDirectory(new File("."));
         System.out.println(treeHash);
     }
 
-    // Represents a tree entry for proper sorting
+    // Create a commit object
+    private static void createCommit(String[] args) throws Exception {
+        // Parse arguments
+        String treeHash = args[1];
+        String message = "";
+        String parentHash = "";
+        
+        // Get parent hash and message from flags
+        for (int i = 2; i < args.length; i += 2) {
+            if (args[i].equals("-p")) {
+                parentHash = args[i + 1];
+            } else if (args[i].equals("-m")) {
+                message = args[i + 1];
+            }
+        }
+
+        // Build commit content
+        StringBuilder commit = new StringBuilder();
+        commit.append("tree ").append(treeHash).append("\n");
+        
+        if (!parentHash.isEmpty()) {
+            commit.append("parent ").append(parentHash).append("\n");
+        }
+
+        // Add author and committer info
+        String timestamp = getGitTimestamp();
+        String userInfo = "Your Name <you@example.com> " + timestamp;
+        
+        commit.append("author ").append(userInfo).append("\n");
+        commit.append("committer ").append(userInfo).append("\n");
+        commit.append("\n").append(message).append("\n");
+
+        // Create and save commit object
+        byte[] content = commit.toString().getBytes();
+        String header = "commit " + content.length + "\0";
+        byte[] fullContent = new byte[header.getBytes().length + content.length];
+        System.arraycopy(header.getBytes(), 0, fullContent, 0, header.getBytes().length);
+        System.arraycopy(content, 0, fullContent, header.getBytes().length, content.length);
+
+        String hash = calculateSHA1(fullContent);
+        saveCompressedFile(hash, fullContent);
+        
+        System.out.println(hash);
+    }
+
+    // Helper class for tree entries
     private static class TreeEntry {
         String mode;
         String name;
@@ -148,7 +197,6 @@ public class Main {
             this.hash = hash;
         }
         
-        // Convert entry to bytes in git format
         byte[] toBytes() {
             ByteArrayOutputStream bytes = new ByteArrayOutputStream();
             try {
@@ -160,69 +208,61 @@ public class Main {
             return bytes.toByteArray();
         }
         
-        // Git's special sorting: trees (40000) get a special character prefix
         String getSortKey() {
             return mode.equals("40000") ? name + "/" : name;
         }
     }
 
-    // Helper to write a tree for a directory
+    // Create tree object for a directory
     private static String writeTreeForDirectory(File dir) throws IOException, NoSuchAlgorithmException {
-        // Keep track of everything in the tree
         List<TreeEntry> entries = new ArrayList<>();
         
-        // Look at all files and folders
-        File[] stuff = dir.listFiles();
-        if (stuff != null) {
-            for (File thing : stuff) {
-                // Skip the .git folder!
-                if (thing.getName().equals(GIT_FOLDER)) {
+        // Process all files and directories
+        File[] items = dir.listFiles();
+        if (items != null) {
+            for (File item : items) {
+                if (item.getName().equals(GIT_FOLDER)) {
                     continue;
                 }
 
-                // Figure out what kind of thing it is
                 String mode;
                 String hash;
                 
-                if (thing.isDirectory()) {
-                    // It's a folder - make a tree for it
+                if (item.isDirectory()) {
                     mode = "40000";
-                    hash = writeTreeForDirectory(thing);
+                    hash = writeTreeForDirectory(item);
                 } else {
-                    // It's a file - make a blob for it
                     mode = "100644";
-                    hash = makeBlob(thing);
+                    hash = makeBlob(item);
                 }
                 
-                // Save the entry info
-                entries.add(new TreeEntry(mode, thing.getName(), hexToBytes(hash)));
+                entries.add(new TreeEntry(mode, item.getName(), hexToBytes(hash)));
             }
         }
 
-        // Sort entries using Git's sorting rules
+        // Sort entries
         entries.sort((a, b) -> a.getSortKey().compareTo(b.getSortKey()));
         
-        // Build the tree content
+        // Create tree content
         ByteArrayOutputStream treeContent = new ByteArrayOutputStream();
         for (TreeEntry entry : entries) {
             treeContent.write(entry.toBytes());
         }
         
-        // Add the header
+        // Add header and save
         byte[] contentBytes = treeContent.toByteArray();
         String header = "tree " + contentBytes.length + "\0";
         ByteArrayOutputStream fullContent = new ByteArrayOutputStream();
         fullContent.write(header.getBytes());
         fullContent.write(contentBytes);
         
-        // Save it and return the hash
         byte[] finalContent = fullContent.toByteArray();
         String hash = calculateSHA1(finalContent);
         saveCompressedFile(hash, finalContent);
         return hash;
     }
 
-    // Helper to make a blob object from a file
+    // Create blob object from file
     private static String makeBlob(File file) throws IOException, NoSuchAlgorithmException {
         byte[] content = Files.readAllBytes(file.toPath());
         String header = "blob " + content.length + "\0";
@@ -236,7 +276,15 @@ public class Main {
         return hash;
     }
 
-    // Helper to read a compressed file
+    // Get timestamp in git format
+    private static String getGitTimestamp() {
+        SimpleDateFormat format = new SimpleDateFormat("s z");
+        format.setTimeZone(TimeZone.getTimeZone("UTC"));
+        long timestamp = System.currentTimeMillis() / 1000;
+        return timestamp + " " + format.format(new Date()).replace("UTC", "+0000");
+    }
+
+    // Read compressed git object
     private static byte[] readCompressedFile(File file) throws IOException {
         try (
             FileInputStream fileIn = new FileInputStream(file);
@@ -252,32 +300,29 @@ public class Main {
         }
     }
 
-    // Helper to save a compressed file
+    // Save compressed git object
     private static void saveCompressedFile(String hash, byte[] content) throws IOException {
-        // Compress the content
-        Deflater squeezer = new Deflater();
-        squeezer.setInput(content);
-        squeezer.finish();
+        Deflater deflater = new Deflater();
+        deflater.setInput(content);
+        deflater.finish();
         
         ByteArrayOutputStream compressed = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
-        while (!squeezer.finished()) {
-            int count = squeezer.deflate(buffer);
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
             compressed.write(buffer, 0, count);
         }
         
-        // Make the folder if needed
         String folder = GIT_FOLDER + "/objects/" + hash.substring(0, 2);
         new File(folder).mkdirs();
         
-        // Save the file
         String filename = folder + "/" + hash.substring(2);
         try (FileOutputStream out = new FileOutputStream(filename)) {
             out.write(compressed.toByteArray());
         }
     }
 
-    // Helper to calculate SHA1 hash
+    // Calculate SHA1 hash
     private static String calculateSHA1(byte[] content) throws NoSuchAlgorithmException {
         MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
         byte[] hash = sha1.digest(content);
@@ -289,7 +334,7 @@ public class Main {
         return result.toString();
     }
 
-    // Helper to convert hex string to bytes properly
+    // Convert hex string to bytes
     private static byte[] hexToBytes(String hex) {
         byte[] bytes = new byte[hex.length() / 2];
         for (int i = 0; i < hex.length(); i += 2) {
